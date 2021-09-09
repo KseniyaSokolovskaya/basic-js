369f7ca6d333ec8f2ffbf55aa46536fd5877cfef
KseniyaSokolovskaya <deff.1990@gmail.com>
2021-09-09 18:48:55 +0300

    complete carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,18 +6,21 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (Number(sampleActivity) > 0 && Number(sampleActivity) < 15 && typeof sampleActivity === 'string') {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (Math.LN2 / HALF_LIFE_PERIOD));
+  } else {
+    return false;
+  }
 }
